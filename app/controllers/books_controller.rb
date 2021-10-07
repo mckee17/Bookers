c2b598fbaa6cdda201f499c457bbe795ad4b9520
mckee17 <hayate017@gmail.com>
2021-10-07 04:07:12 +0000

    [add]roottop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,38 @@
 class BooksController < ApplicationController
-  
+
   def top
   end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def new
-    #不要
+    #不要？
   end
-  
+
   def create
+    book = Book.new
+    book.save
+
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   #privete
-  
-  
+  private
+  def book_params
+    params.require(:Book).permit(:title,:body)
+  end
 end
