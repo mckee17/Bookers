80a4463eaae4727bc270e4252a7355308ac6e1bd
mckee17 <hayate017@gmail.com>
2021-10-09 05:52:05 +0000

    [add]path>resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'books#top'
   get '/' => 'books#top'
-
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show',as:'SHOW'
-  get 'books/:id/edit' => 'books#edit',as:'EDIT'
-  patch 'books/:id/' => 'books#update',as:'UPDATE'
-  delete 'books/:id' => 'books#destroy',as:'DELETE'
+  
+  resources :books, only: [:index,:show,:create,:edit,:update,:destroy]
 end
